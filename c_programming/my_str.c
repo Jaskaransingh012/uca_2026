4cf75c70e429c41d8a01b032b7c4bdad3b5eb902
jaskaransingh012 <jaskaransingh_1@outlook.com>
2026-07-18 10:47:41 +0530

    my_str week assignment c

---
+++ c_programming/my_str.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int my_strlen(const char *str) {
+    int length = 0;
+
+    while (str[length] != '\0') {
+        length++;
+    }
+
+    return length;
+}
+
+int main() {
+    char str[100];
+
+    printf("Enter: ");
+    scanf("%99s", str);
+
+    printf("Length = %d\n", my_strlen(str));
+
+    return 0;
+}

393a44351fe641d7a05210e96ccc742ed1963b3d
jaskaransingh012 <jaskaransingh_1@outlook.com>
2026-08-03 08:07:44 +0530

    sign Problem

---
+++ c_programming/bit_manipulation/sign.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * sign - return 1 if positive, 0 if zero, and -1 if negative
+ *  Examples: sign(130) = 1
+ *            sign(-23) = -1
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 10
+ *  Rating: 2
+ */
+int sign(int x) {
+
+    return (x >> 31) | (!!x);
+}
+
+int main() {
+
+    printf("%d\n", sign(4));
+}

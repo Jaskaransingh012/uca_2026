47847c7d93848876e5a60bc0fc9a735fb7c0d0d7
jaskaransingh012 <jaskaransingh_1@outlook.com>
2026-08-03 10:58:22 +0530

    conditional problem

---
+++ c_programming/bit_manipulation/conditional.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * conditional - same as x ? y : z
+ *   Example: conditional(2,4,5) = 4
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 16
+ *   Rating: 3
+ */
+int conditional(int x, int y, int z) {
+    int mask = ~(!!x) + 1;
+    return (mask & y) | (~mask & z);
+}
+
+int main(){
+
+    printf("%d\n",conditional(9,3,6));
+    printf("%d\n",conditional(0,3,6));
+
+}
